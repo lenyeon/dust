7d6dd686b35b18a5a90f33af372b75c289671bb3
lenyeon <lenyeon@gmail.com>
2016-05-28 19:31:16 +0900

    .

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 100)
+    content = models.TextField()
+    photo = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now_add = True)
+    def get_absolust_url(self):
+        return reverse('blog:post_detail', args=[self.pk])
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    author = models.CharField(max_length=20)
+    message = models.TextField()
+
---
+++ blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post
+from blog.forms import CommentForm
+from django.http import HttpResponse, Http404
+
+def post_list(request):
+    post_list = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'post_list':post_list})
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post' : post})
+
+def comment_new(request, post_pk):
+
+    post = get_object_or_404(Post, pk=post_pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect(post)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_new.html', {'form':form})
